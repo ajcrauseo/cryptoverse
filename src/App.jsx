474bd7f98bbd7413e24ce8ae81b9a5c720ad6858
Antony Cabeza <antonycabeza@gmail.com>
2023-02-16 13:36:05 -0400

    Layout

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+import {
+  Home,
+  Exchanges,
+  Cryptocurrencies,
+  Cryptodetails,
+  News,
+} from './pages';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinId' element={<Cryptodetails />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            Cryptoverse <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Cryptocurrencies from './Cryptocurrencies';
+import Cryptodetails from './Cryptodetails';
+import Exchanges from './Exchanges';
+import Home from './Home';
+import News from './News';
+
+export { Cryptocurrencies, Cryptodetails, News, Home, Exchanges };
